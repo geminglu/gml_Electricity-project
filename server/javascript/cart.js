d924a185877341f1f04c5dc3df5fb976ff953894
gml <18451487672@qq.com>
2019-12-10 07:58:08 +0800

    第二次修改

---
+++ server/javascript/cart.js
@@ -0,0 +1,186 @@
+import "./jquery-1.12.4.js";
+import "./Ajax.js";
+import "./cookie.js";
+
+
+$(".top1").load("../html/header.html")
+$(".sidebar1").load("../html/sidebar.html")
+
+class Car{
+    constructor(){
+        // 获取元素
+        this.url = "goods.json";
+        this.tby = document.querySelector(".tbody");
+        this.totalValue = document.querySelector(".totalValue");
+        this.load();
+        this.addEvent();
+    }
+    // 请求数据
+    load(){
+        ajaxGet(this.url,(res)=>{
+            // 解析成数组
+            this.res = JSON.parse(res);
+            this.getCookie();
+        },{
+
+        });
+    }
+    // 获取cookie
+    getCookie(){
+        // 如果cookie里面有数据就解析数据如果没有数据就返回一个空数组
+        this.goods = getCookie("goodsCookie") ? JSON.parse(getCookie("goodsCookie")) : [];
+        this.display();
+    }
+    // 渲染页面
+    display(){
+        var str = "";
+        for (var i=0;i<this.goods.length;i++) {
+            for (var j=0;j<this.res.length;j++) {
+                // 如果cookie里的数据和json的数据匹配成功就渲染页面
+                if (this.goods[i].id === this.res[j].goodsId){
+                    str += `<tr id="${this.res[j].goodsId}" class="list">
+                        <td class="check"><input type="checkbox" class="checkbox" checked /></td>
+                        <td><img src="${this.res[j].img}" /></td>
+                        <td>${this.res[j].name}</td>
+                        <td class="jg">${this.res[j].price}</td>
+                        <td><input type="number" class="num" min=1 value="${this.goods[i].num}" /></td>
+                        <td class="subtotal"><span>00.00</span></td>
+                        <td><input type="button" class="del" value="删除"></td>
+                    </tr>`;
+                }
+            }
+        }
+        this.tby.innerHTML = str;
+        // 小计,必须要在页面数据加载成功后执行，否则获取不到页面元素
+        this.total();
+        // 选项
+        this.Operation();
+        // 总价
+        this.totalPrices();
+    }
+    addEvent(){
+        this.tby.onclick = (eve)=> {
+            var e = eve || window.event;
+            this.target = e.target || e.srcElement;
+            // 删除按钮
+            if (this.target.className == "del") {
+                // 删除父级
+                this.target.parentNode.parentNode.remove();
+                this.delCookie(this.target);
+            }
+            // 数量
+            if (this.target.className == "num") {
+                this.num_Vlu = this.target.value;
+                this.upCookie();
+            }
+            // 重新计算小计
+            this.total();
+        }
+    }
+    delCookie(){
+        // 每次点击都需要获取新的cookie，因为商品列表修改后没有刷新购物车页面就修改了购物车的数据，这时修改的还是旧的cookie数据，所以在修改时就要获取最新的cookie
+        this.co = JSON.parse(getCookie("goodsCookie"))
+        // 用cookie的数据与每一个购物车列表对比
+        for (var i=0;i<this.co.length;i++) {
+            if (this.target.parentNode.parentNode.id === this.co[i].id) {
+                // 如果匹配到了就删除cookie里的相应数据
+                this.co.splice([i],1)
+                // 数据只有一条不会重复的，只要匹配到就不用在匹配了
+                break;
+            }
+        }
+        // 将新的cookie数据存到cookie里
+        setCookie("goodsCookie",JSON.stringify(this.co))
+    }
+    upCookie(){
+        // 获取新cookie数据
+        this.uco = JSON.parse(getCookie("goodsCookie"));
+        // 用cookie的数据与每一个购物车列表对比
+        for (var i=0;i<this.uco.length;i++) {
+            if (this.target.parentNode.parentNode.id === this.uco[i].id) {
+                // 如果匹配到了就修改cookie里的num
+                (this.uco)[i].num = this.target.value;
+                // 数据只有一条不会重复的，只要匹配到就不用在匹配了
+                break;
+            }
+        }
+        // 将新的cookie数据存到cookie里
+        setCookie("goodsCookie",JSON.stringify(this.uco))
+    }
+    // 小计
+    total(){
+        this.str = 0;
+        for (var i=0;i<this.tby.children.length;i++) {
+            var j = this.tby.children[i].querySelector(".jg").innerHTML;
+            var s = this.tby.children[i].querySelector(".num").value;
+            // 小计
+            var w = this.tby.children[i].querySelector(".subtotal").querySelector("span").innerHTML = j * s;
+            // 总价
+            this.str += w;
+        }
+        this.totalValue.children[0].innerHTML = this.str;
+    }
+    totalPrices(){
+        console.log("fcbejh");
+
+    }
+    // 选项
+    Operation(){
+        $.fn.extend({
+            // 有一个为false就返回false
+            selectAll:function () {
+                // 默认为true
+                var flag = true;
+                // 遍历这个数组
+                this.each(function (index,item) {
+                    // 如果有一个为false就为false
+                    if (item.checked === false) {
+                        flag = false;
+                    }
+                })
+                return flag;
+            },
+            // 反选
+            toggleSelect:function () {
+                this.each(function (index, item) {
+                    // 复制当前相反的状态
+                    item.checked = !item.checked;
+                })
+            },
+            // 取消
+            cancel:function () {
+                this.each(function (index,item) {
+                    item.checked = false;
+                })
+            }
+        })
+        // 全选
+        $(".checkall").change(function () {
+            $('.checkbox').prop('checked',this.checked);
+            // 总价
+            that.totalPrices();
+        })
+        var that = this;
+        $('.checkbox').change(function () {
+            $(".checkall").prop('checked',$('.checkbox').selectAll());
+            // 总价
+            that.totalPrices();
+        })
+        // 反选
+        $(".antitone").click(function () {
+            $('.checkbox').toggleSelect();
+            // 这两行代码顺序不能乱
+            $(".checkall").prop('checked',$('.checkbox').selectAll());
+            // 总价
+            that.totalPrices();
+        })
+        // 取消
+        $(".cancel").click(function () {
+            $('.checkbox').cancel();
+            $(".checkall").prop('checked',$('.checkbox').selectAll());
+            // 总价
+            that.totalPrices();
+        })
+    }
+}
+new Car;
---
+++ server/javascript/index.js
@@ -0,0 +1,5 @@
+import "./jquery-1.12.4.js";
+import "./Ajax.js";
+import "./cookie.js";
+
+console.log("dd")
